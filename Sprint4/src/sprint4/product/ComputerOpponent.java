9d89082140bd643e916e92eec32a057e32e92a49
jsb58p <jacobbiddinger@gmail.com>
2025-11-11 18:39:03 -0600

    Added Radio Buttons for Computer/Human opponent, formatting updates.

---
+++ Sprint4/src/sprint4/product/ComputerOpponent.java
@@ -0,0 +1,20 @@
+package sprint4.product;
+
+/**
+ * Abstract base class for SOS Computer Opponent
+ */
+public abstract class ComputerOpponent {
+	
+	protected SOSGame game;
+	
+	/**
+	 * Creates a new computer opponent logic instance.
+	 * 
+	 * @param game the SOS game instance
+	 */
+	public ComputerOpponent(SOSGame game) {
+		this.game = game;
+	}
+	
+	
+}
